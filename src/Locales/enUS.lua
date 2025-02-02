18e4537e80d32f7e86b32374d59a51c90b2f028e
Anthony Tabano <tabano86@gmail.com>
2025-02-02 13:46:39 -0500

    Refactor addon structure for clarity and load order.
    Updated `Embeds.xml` to better categorize and clarify file organization, ensuring proper library load order. Modified `SleekChat.toc` to reference `Embeds.xml` for streamlined library handling and updated comments for improved readability.

--- src/Locales/enUS.lua
+++ src/Locales/enUS.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("SleekChat", "enUS", true)
+local L = LibStub('AceLocale-3.0'):NewLocale("SleekChat", "enUS", true, true)
 
 L.general = "General"
 L.appearance = "Appearance"
